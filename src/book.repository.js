1c7c8ae37847e8e82924d8cd647ec0bc72178134
sbab917 <odite972@hotmail.fr>
2019-10-10 00:02:35 +0200

    Function getTotalPrice

--- src/book.repository.js
+++ src/book.repository.js
@@ -1,62 +1,67 @@
 class BookRepository {
 
-    /**
-     * @param db
-     */
-    constructor(db) {
-        this.db = db;
+  /**
+  * @param db
+  */
+  constructor(db) {
+    this.db = db;
+  }
+
+  save (book) {
+    this.db.get('books').push(book).write();
+  }
+
+  /**
+  * Nombre total de livre
+  */
+  getTotalCount() {
+    return this.db.get('books').size().value();
+  }
+
+  /**
+  * Somme du prix de tous les livre
+  */
+  getTotalPrice() {
+    let result = 0;
+    let price = this.db.get('books').map('price').value();
+    for(let i = 0; i<price.length; i++){
+      result += price[i];
     }
-
-    save (book) {
-        this.db.get('books').push(book).write();
-    }
-
-    /**
-     * Nombre total de livre
-     */
-    getTotalCount() {
-      return this.db.get('books').size().value();
-    }
-
-    /**
-     * Somme du prix de tous les livre
-     */
-    getTotalPrice() {
-
-    }
+    return result;
+  }
 
 
-    /**
-     * Retourne un livre
-     */
-    getBookByName(bookName) {
+  /**
+  * Retourne un livre
+  */
+  getBookByName(bookName) {
 
-    }
+  }
 
-    /**
-     * Nombre de livre ajouté par mois
-     *
-     *  [
-     *      {
-     *          year: 2017,
-     *          month, 2,
-     *          count, 129,
-     *          count_cumulative: 129
-     *      },
-     *      {
-     *          year: 2017,
-     *          month, 3,
-     *          count, 200,
-     *          count_cumulative: 329
-     *      },
-     *      ....
-     *  ]
-     */
-    getCountBookAddedByMont(bookName) {
+  /**
+  * Nombre de livre ajouté par mois
+  *
+  *  [
+    *      {
+      *          year: 2017,
+      *          month, 2,
+      *          count, 129,
+      *          count_cumulative: 129
+      *      },
+      *      {
+        *          year: 2017,
+        *          month, 3,
+        *          count, 200,
+        *          count_cumulative: 329
+        *      },
+        *      ....
+        *  ]
+        */
+        getCountBookAddedByMont(bookName) {
 
-    }
+        }
 
-}
+      }
 
 
-module.exports = BookRepository;
+      module.exports = BookRepository;
