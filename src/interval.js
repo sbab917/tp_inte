a5529cdbf642f4f40d97810e83229972ead8e9f7
sbab917 <odite972@hotmail.fr>
2019-10-09 22:59:54 +0200

    Function Exclusion

--- src/interval.js
+++ src/interval.js
@@ -1,151 +1,191 @@
 class Interval {
-    constructor(start, end) {
-        this.start = start;
-        this.end = end
+  constructor(start, end) {
+    this.start = start;
+    this.end = end
+  }
+
+  toString() {
+    return "[" + this.start + "," + this.end + "]";
+  }
+
+  /**
+  * Exemple 1 :
+  *      interval1 =                          ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  *      interval2 =                                  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  *      interval1.overlaps(interval2) => true
+  *
+  * Exemple 2 :
+  *      interval1 =                          ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  *      interval2 =                                                       ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  *      interval1.overlaps(interval2) => false
+  *
+  * @param {Interval} interval
+  * @returns {boolean}
+  */
+  overlaps(interval) {
+    return this.end > interval.start && this.start < interval.end;
+  }
+
+  /**
+  * Retourne true si cet interval contient le paramètre interval
+  *
+  * Exemple 1 :
+  *      interval1 =                          ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  *      interval2 =                                  ▓▓▓▓▓▓▓▓▓▓▓▓▓
+  *      interval1.includes(interval2) => true
+  *
+  * Exemple 2 :
+  *      interval1 =                          ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  *      interval2 =                              ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  *      interval1.includes(interval2) => false
+  *
+  * @param {Interval} interval
+  * @returns {boolean}
+  */
+  includes(interval) {
+    return (interval.start>= this.start && interval.end<= this.end);
+  };
+
+  /**
+  * Retourne l'union de deux intervals
+  *
+  * Exemple 1 :
+  *      interval1 =                          ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  *      interval2 =                                              ▓▓▓▓▓▓▓▓▓▓▓▓▓
+  *      interval1.union(interval2) =>        ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+  *
+  * Exemple 2 :
+  *      interval1 =                          ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  *      interval2 =                                                      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  *      interval1.union(interval2) =>        ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+  *
+  * @param {Interval} interval
+  * @returns {Interval[]}
+  */
+  union(interval) {
+    let newStart = this.start;
+    let newEnd = this.end;
+    if(this.end < interval.start){
+      return [this, interval];
+    }
+    else if( interval.end < this.start){
+      return[interval,this];
     }
 
-    toString() {
-        return "[" + this.start + "," + this.end + "]";
+    if(interval.start > this.start){
+      newStart= this.start;
     }
-
-    /**
-     * Exemple 1 :
-     *      interval1 =                          ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval2 =                                  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval1.overlaps(interval2) => true
-     *
-     * Exemple 2 :
-     *      interval1 =                          ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval2 =                                                       ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval1.overlaps(interval2) => false
-     *
-     * @param {Interval} interval
-     * @returns {boolean}
-     */
-    overlaps(interval) {
-        return this.end > interval.start && this.start < interval.end;
+    else {
+      newStart = interval.start;
     }
+    if(interval.end < this.end){
+      newEnd = this.end;
+    }
+    else {
+      newEnd = interval.end;
+    }
+    return new Interval(newStart,newEnd);
+  };
 
-    /**
-     * Retourne true si cet interval contient le paramètre interval
-     *
-     * Exemple 1 :
-     *      interval1 =                          ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval2 =                                  ▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval1.includes(interval2) => true
-     *
-     * Exemple 2 :
-     *      interval1 =                          ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval2 =                              ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval1.includes(interval2) => false
-     *
-     * @param {Interval} interval
-     * @returns {boolean}
-     */
-    includes(interval) {
-        return (interval.start>= this.start && interval.end<= this.end);
-    };
+  /**
+  * Retourne l'intersection de deux intervals
+  *
+  * Exemple 1 :
+  *      interval1 =                          ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  *      interval2 =                                              ▓▓▓▓▓▓▓▓▓▓▓▓▓
+  *      interval1.intersection(interval2) =>                     ▒▒▒▒▒
+  *
+  * Exemple 2 :
+  *      interval1 =                          ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  *      interval2 =                                                      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  *      interval1.intersection(interval2) => <tableau vide>
+  *
+  * @param {Interval} interval
+  * @returns {Interval|null}
+  */
+  intersection(interval) {
+    let newStart = null;
+    let newEnd = null;
+    if(this.end > interval.start && this.start < interval.start){
+      newStart = interval.start;
+      for (let i = newStart; i <= this.end; i++){
+        if(i > interval.end){
+          break;
+        }
+        newEnd = i;
+      }
+    }else if (interval.end > this.start && interval.start < this.start){
+      newStart = this.start;
+      for(let j = 0 ; j <= interval.end; j++){
+        if(j > this.end){
+          break;
+        }
+        newEnd = j;
+      }
+    }
+    if(newStart == null) return null;
+    return new Interval(newStart,newEnd);
+  };
 
-    /**
-     * Retourne l'union de deux intervals
-     *
-     * Exemple 1 :
-     *      interval1 =                          ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval2 =                                              ▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval1.union(interval2) =>        ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-     *
-     * Exemple 2 :
-     *      interval1 =                          ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval2 =                                                      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval1.union(interval2) =>        ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-     *
-     * @param {Interval} interval
-     * @returns {Interval[]}
-     */
-    union(interval) {
-        let newStart = this.start;
-        let newEnd = this.end;
-        if(this.end < interval.start){
-            return [this, interval];
-        }
-        else if( interval.end < this.start){
-            return[interval,this];
-        } 	
-                
-        if(interval.start > this.start){
-            newStart= this.start;
-        }
-        else {
-            newStart = interval.start;
-        } 
-        if(interval.end < this.end){
-            newEnd = this.end;
-        }
-        else {
-            newEnd = interval.end;
-        }
-        return new Interval(newStart,newEnd);
-    };
-
-    /**
-     * Retourne l'intersection de deux intervals
-     *
-     * Exemple 1 :
-     *      interval1 =                          ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval2 =                                              ▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval1.intersection(interval2) =>                     ▒▒▒▒▒
-     *
-     * Exemple 2 :
-     *      interval1 =                          ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval2 =                                                      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval1.intersection(interval2) => <tableau vide>
-     *
-     * @param {Interval} interval
-     * @returns {Interval|null}
-     */
-    intersection(interval) {
-        let newStart = null;
-        let newEnd = null;	
-        if(this.end > interval.start && this.start < interval.start){
-            newStart = interval.start;
-            for (let i = newStart; i <= this.end; i++){
-                if(i > interval.end){ 
-                    break;
-                }
-                newEnd = i;
-            }
-        }else if (interval.end > this.start && interval.start < this.start){
-            newStart = this.start;
-            for(let j = 0 ; j <= interval.end; j++){
-                if(j > this.end){
-                    break;
-                }
-                newEnd = j;
-            }
-        }
-        if(newStart == null) return null;
-        return new Interval(newStart,newEnd);
-    };
-
-    /**
-     * Retourne l'exclusion de deux intervals
-     *
-     * Exemple 1 :
-     *      interval1 =                          ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval2 =                                              ▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval1.exclusion(interval2) =>    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒     ▒▒▒▒▒▒▒▒
-     *
-     * Exemple 2 :
-     *      interval1 =                          ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval2 =                                                      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-     *      interval1.exclusion(interval2) =>    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-     *
-     * @param {Interval} interval
-     * @returns {Interval[]}
-     */
-    exclusion(interval) {
-
-    };
+  /**
+  * Retourne l'exclusion de deux intervals
+  *
+  * Exemple 1 :
+  *      interval1 =                          ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  *      interval2 =                                              ▓▓▓▓▓▓▓▓▓▓▓▓▓
+  *      interval1.exclusion(interval2) =>    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒     ▒▒▒▒▒▒▒▒
+  *
+  * Exemple 2 :
+  *      interval1 =                          ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  *      interval2 =                                                      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+  *      interval1.exclusion(interval2) =>    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+  *
+  * @param {Interval} interval
+  * @returns {Interval[]}
+  */
+  exclusion(interval) {
+    if (this.start == interval.start && this.end == interval.end){
+      return null;
+    }
+    if(this.end < interval.start ){
+      return [this, interval];
+    }
+    else if(interval.end < this.start){
+      return[interval,this];
+    }
+    let inter_start1=null;
+    let inter_start2=null;
+    let inter_end1=null;
+    let inter_end2=null;
+    let inter = this.intersection(interval);
+    if((this.end>interval.start && interval.start>this.start) && interval.end> this.end){
+      inter_start1 = this.start;
+      inter_end1 = inter.start;
+      inter_start2 = inter.end;
+      inter_end2 = interval.end;
+    }
+    else if((interval.end> this.start && this.start> interval.start) && this.end> interval.end){
+      inter_start1 = interval.start;
+      inter_end1 = inter.start;
+      inter_start2 = inter.end;
+      inter_end2 = this.end;
+    }
+    else if((this.end>interval.start && interval.start>this.start) && interval.end< this.end){
+      inter_start1 = this.start;
+      inter_end1 = inter.start;
+      inter_start2 = inter.end;
+      inter_end2 = this.end;
+    }
+    else if ((interval.end> this.start && this.start> interval.start) && this.end < interval.end){
+      inter_start1 = interval.start;
+      inter_end1 = inter.start;
+      inter_start2 = inter.end;
+      inter_end2 = interval.end;
+    }
+    let res1 = new Interval(inter_start1,inter_end1);
+    let res2 = new Interval(inter_start2,inter_end2);
+    return [res1,res2]
+  };
 }
 
 module.exports = Interval;
