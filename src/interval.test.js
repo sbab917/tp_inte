5ce917a4f4d4bf4a6a60bc877dbadfc30bf9e5ed
sbab917 <odite972@hotmail.fr>
2019-10-09 20:30:55 +0200

    Function Includes

--- src/interval.test.js
+++ src/interval.test.js
@@ -1,14 +1,33 @@
 const Interval = require('./interval');
 
 describe('overlaps', function () {
-    let intervalGenerale = new Interval(5,6);
+    let intervalGeneral = new Interval(5,6);
 
     test('Test interval overlaps true', () => {
-	let interval2 = new Interval(5,6);
-        expect(intervalGenerale.overlaps(interval2)).toBe(true)
+	let interval = new Interval(5,6);
+        expect(intervalGeneral.overlaps(interval)).toBe(true)
     });
     test('Test interval overlaps false', () => {
-	let interval2 = new Interval(1,3);
-        expect(intervalGenerale.overlaps(interval2)).toBe(false)
+	let interval = new Interval(1,3);
+        expect(intervalGeneral.overlaps(interval)).toBe(false)
+    });
+});
+describe('includes', function () {
+    let intervalGeneral = new Interval(4,8);
+    test('Test interval with total inclusion send true', () => {
+	let interval = new Interval(4,8);
+        expect(intervalGeneral.includes(interval)).toBe(true);
+    });
+    test('Test interval includes send true', () => {
+	let interval = new Interval(5,6);
+        expect(intervalGeneral.includes(interval)).toBe(true);
+    });
+    test('Test interval with total exclusion send false', () => {
+	let interval = new Interval(1,3);
+        expect(intervalGeneral.includes(interval)).toBe(false);
+    });
+    test('Test interval includes send false', () => {
+	let interval = new Interval(1,5);
+        expect(intervalGeneral.includes(interval)).toBe(false);
     });
 });
